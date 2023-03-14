f049b94ec6e0176572781c9bdfc91946d58547b4
Marcos Cordeiro <marcoscbrito@gmail.com>
2023-03-14 19:34:33 -0300

    atualização para rodar no IntelliJ

--- src/main/java/br/com/fiap/progamer/ProgamerApplication.java
+++ src/main/java/br/com/fiap/progamer/ProgamerApplication.java
@@ -1,34 +1,43 @@
 package br.com.fiap.progamer;
 
-import java.util.Arrays;
-
 import javax.faces.webapp.FacesServlet;
 import javax.servlet.ServletContext;
 
+import com.sun.faces.config.ConfigureListener;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.ServletContextAware;
 
-@SpringBootApplication
-public class ProgamerApplication {
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class ProgamerApplication implements ServletContextAware {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProgamerApplication.class, args);
+		SpringApplication.run(ProgamerApplication.class);
 	}
 
 	@Bean
-	ServletRegistrationBean<FacesServlet> jsfServletRegistration(ServletContext servletContext) {
-		// spring boot only works if this is set
-		servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+	ServletRegistrationBean<FacesServlet> facesServletRegistration() {
+		ServletRegistrationBean<FacesServlet> registration = new ServletRegistrationBean<FacesServlet>(
+				new FacesServlet(), "*.xhtml");
+		registration.setLoadOnStartup(1);
+		return registration;
+	}
 
-		// FacesServlet registration
-		ServletRegistrationBean<FacesServlet> srb = new ServletRegistrationBean<FacesServlet>();
-		srb.setServlet(new FacesServlet());
-		srb.setUrlMappings(Arrays.asList("*.xhtml"));
-		srb.addUrlMappings("/console/*");
-		srb.setLoadOnStartup(1);
-		return srb;
+	@Bean
+	ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+		return new ServletListenerRegistrationBean<ConfigureListener>(new ConfigureListener());
+	}
+
+	@Override
+	public void setServletContext(ServletContext servletContext) {
+		servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
 	}
 
 }
